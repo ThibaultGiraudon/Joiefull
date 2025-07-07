b44b4d33b6026c5b5ef19d5d2de1668e4ce41531
ThibaultGiraudon <thibault.giraudon@gmail.com>
2025-07-07 10:59:19 +0200

    Initial Commit

---
+++ Joiefull/JoiefullApp.swift
@@ -0,0 +1,17 @@
+//
+//  JoiefullApp.swift
+//  Joiefull
+//
+//  Created by Thibault Giraudon on 07/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct JoiefullApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
