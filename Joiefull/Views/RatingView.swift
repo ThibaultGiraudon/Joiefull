1b22cc40d7dc259724703160c9614a905cba755f
ThibaultGiraudon <thibault.giraudon@gmail.com>
2025-07-08 17:28:21 +0200

    Add france to localization

---
+++ Joiefull/Views/RatingView.swift
@@ -0,0 +1,18 @@
+//
+//  RatingView.swift
+//  Joiefull
+//
+//  Created by Thibault Giraudon on 08/07/2025.
+//
+
+import SwiftUI
+
+struct RatingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RatingView()
+}
