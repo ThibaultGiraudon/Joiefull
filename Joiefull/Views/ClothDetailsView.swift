ad2e9ecb4e0c19d526f04e4b9cc27752eea36db0
ThibaultGiraudon <thibault.giraudon@gmail.com>
2025-07-08 15:14:50 +0200

    Add share button

--- Joiefull/Views/ClothDetailsView.swift
+++ Joiefull/Views/ClothDetailsView.swift
@@ -23,36 +23,43 @@ struct ClothDetailsView: View {
         ScrollView(showsIndicators: false) {
             VStack(alignment: .leading) {
                 AsyncImage(url: cloth.picture.url) { image in
-                    ZStack(alignment: .bottomTrailing) {
-                        image
-                            .resizable()
-                            .aspectRatio(contentMode: .fill)
-                            .frame(height: height)
-                            .clipped()
-                            .clipShape(RoundedRectangle(cornerRadius: 20))
-                            .accessibilityLabel(cloth.picture.description)
-                        
-                        HStack {
-                            Image(systemName: cloth.isLiked ? "heart.fill" : "heart")
-                            Text("\(cloth.likes)")
-                        }
-                        .padding(5)
-                        .background {
-                            Capsule()
-                                .fill(.white)
-                        }
-                        .padding(10)
-                        .onTapGesture(count: 2) {
-                            cloth.isLiked.toggle()
-                            if cloth.isLiked == true {
-                                cloth.likes += 1
-                            } else {
-                                cloth.likes -= 1
+                    ZStack(alignment: .topTrailing) {
+                        ZStack(alignment: .bottomTrailing) {
+                            image
+                                .resizable()
+                                .aspectRatio(contentMode: .fill)
+                                .frame(height: height)
+                                .clipped()
+                                .clipShape(RoundedRectangle(cornerRadius: 20))
+                                .accessibilityLabel(cloth.picture.description)
+                            
+                            HStack {
+                                Image(systemName: cloth.isLiked ? "heart.fill" : "heart")
+                                Text("\(cloth.likes)")
                             }
+                            .padding(5)
+                            .background {
+                                Capsule()
+                                    .fill(.white)
+                            }
+                            .padding(10)
+                            .onTapGesture(count: 2) {
+                                cloth.isLiked.toggle()
+                                if cloth.isLiked == true {
+                                    cloth.likes += 1
+                                } else {
+                                    cloth.likes -= 1
+                                }
+                            }
+                            .accessibilityElement()
+                            .accessibilityLabel(cloth.isLiked ? "Vêtement aimé" : "Vêtement non aimé")
+                            .accessibilityHint("Double-tape pour \(cloth.isLiked ? "retirer le like" : "ajouter un like")")
                         }
-                        .accessibilityElement()
-                        .accessibilityLabel(cloth.isLiked ? "Vêtement aimé" : "Vêtement non aimé")
-                        .accessibilityHint("Double-tape pour \(cloth.isLiked ? "retirer le like" : "ajouter un like")")
+                        ShareLink(item: cloth.picture.url, subject: Text("Jette un oeil à cet atricle"), message: Text(cloth.name)) {
+                            Image("share")
+                        }
+                        .padding()
+                        .foregroundStyle(.black)
                     }
                 } placeholder: {
                     ProgressView()
